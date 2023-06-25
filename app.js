6b14d8866fcc7f896438ebb0661af10ff663581d
Amalascrobits <90767720+Amala06@users.noreply.github.com>
2023-06-26 00:45:41 +0530

    first commit

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express=require('express');
 const app=express();
 app.use(express.json());
+const cors = require("cors");
+app.use(cors());
+app.use(
+  cors({
+    origin: '*' ,// Set the allowed origin(s)
+    methods: ["GET", "POST"], // Set the allowed HTTP methods
+    allowedHeaders: ["Content-Type", "Authorization"], // Set the allowed headers
+  })
+);
+
 
 
 module.exports=app;
